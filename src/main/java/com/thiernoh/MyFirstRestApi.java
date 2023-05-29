5edb656a3fd3b015d927cfe9ee63108e30c413dd
Thiernoh <thiernohgradiagram@gmail.com>
2023-05-29 15:02:22 -0400

    my first Rest API

---
+++ src/main/java/com/thiernoh/MyFirstRestApi.java
@@ -0,0 +1,67 @@
+package com.thiernoh;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.Point;
+import java.util.List;
+
+@RestController
+public class MyFirstRestApi {
+
+    @GetMapping("/")
+    public String greet(@RequestParam(value = "name", required = false) String nameTypedByClient) {
+        String greet =
+                (nameTypedByClient == null || nameTypedByClient.isBlank())
+                        ? "Hello World!"
+                        : "Hello " + nameTypedByClient + "!";
+        return greet;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/morning")
+    public String goodMorning() {
+        return "Good Morning!";
+    }
+
+    @GetMapping("/afternoon")
+    public AfternoonGreet goodAfternoon() {
+        return new AfternoonGreet("Good Afternoon!", List.of("Java", "JavaScript", "Python"), new Point(12, 13));
+    }
+
+    @GetMapping("/night")
+    public NightGreet goodNight() {
+        return new NightGreet("Good Night!");
+    }
+
+    record AfternoonGreet(
+            String greet,
+            List<String> favProgrammingLang,
+            Point point
+    ) {}
+
+    class NightGreet {
+        private String greet;
+
+        public NightGreet(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        public void setGreet(String greet) {
+            this.greet = greet;
+        }
+
+        @Override
+        public String toString() {
+            return "NightGreet{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+    }
+}
